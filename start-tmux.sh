5b19acaee09baae5948774c228476f7e2d79ebb9
skugge74 <54121021+skugge74@users.noreply.github.com>
2024-09-09 12:28:07 +0200

    Update start-tmux.sh

--- start-tmux.sh
+++ start-tmux.sh
@@ -8,15 +8,15 @@ fi
 
 CONFIG_FILE="$1"
 
-# Check if the file has a .conf extension
-if [[ ! "$CONFIG_FILE" == *.conf ]]; then
-    echo "Error: Configuration file must have a .conf extension."
-    exit 1
-fi
-
 # Check if the file exists
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "Error: File '$CONFIG_FILE' not found."
+    exit 1
+fi
+
+# Check if the file has a .conf extension
+if [[ ! "$CONFIG_FILE" == *.conf ]]; then
+    echo "Error: Configuration file must have a .conf extension."
     exit 1
 fi
 
